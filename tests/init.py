6664fe6eebdee97ca7eea8aad5b76b0b8bf2386a
Abu Jamal Abdulnasser <122178545+718334537@users.noreply.github.com>
2025-12-31 18:13:14 +0300

    Create init.py

---
+++ tests/init.py
@@ -0,0 +1,10 @@
+"""
+Saba Null Framework - {اسم المجلد}
+الإصدار: 5.0
+"""
+
+__version__ = "5.0"
+__author__ = "Saba Null Team"
+__license__ = "Educational Purposes Only"
+
+# تصدير الوحدات الرئيسية
